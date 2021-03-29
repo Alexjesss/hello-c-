c42bc5f6c6997d28f2e54509194f29f0e2bb3d84
Alexandra Banica <banica9221@hotmail.be>
2021-03-29 12:18:22 +0200

    dotnet

---
+++ helloWorld/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello there,who am i speaking to?");
+            string Alexandra = Console.ReadLine();
+            Console.WriteLine(Alexandra  + ("Is that your name?"));
+            string answer = Console.ReadLine();
+            if (answer == "yes")
+            {
+                Console.WriteLine("Great!");
+            }
+            
+           else
+            {
+                Console.WriteLine("Ok, then I will call you anonymous...");
+            }
+
+            Console.WriteLine(Alexandra + "What is your age?");
+            int age;
+            age = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (age > 27)
+            {
+                Console.WriteLine("You are" + age + "years old");
+            }
+            else if (age == 27)
+            {
+                Console.WriteLine("ok, noice");
+            }
+            else
+            {
+                Console.WriteLine("I hope you are at least above 18...");
+            }
+
+            Console.WriteLine("Today is" + DateTime.Now.ToString());
+
+            
+        }
+    }
+}
